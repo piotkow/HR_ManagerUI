7633bf15476302fe5b594a0d071a12ce0c3a7082
piotkow <piotkow133@student.polsl.pl>
2024-04-02 12:15:54 +0200

    card details

--- src/app/components/employee-card/employee-card.component.ts
+++ src/app/components/employee-card/employee-card.component.ts
@@ -1,28 +1,40 @@
 import { Component } from '@angular/core';
 import { EmployeeApi, EmployeePositionTeamResponse } from '../../../../libs/api-client';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { Subscription } from 'rxjs';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-employee-card',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, AvatarModule, ButtonModule],
   templateUrl: './employee-card.component.html',
   styleUrl: './employee-card.component.css'
 })
 export class EmployeeCardComponent {
   employeeID: string | null = '';
   employee?: EmployeePositionTeamResponse;
+  routerSubscription?: Subscription;
 
   constructor(
     private employeeApi: EmployeeApi,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ){}
 
-  ngOnInit(){
+  
+  ngOnInit(): void {
     this.employeeID= this.activatedRoute.snapshot.paramMap.get('id');
     this.getEmployee();
     Number(this.employeeID);
+    this.router.events.subscribe(eventResult => {
+      if (eventResult instanceof NavigationEnd) {
+        this.employeeID = this.activatedRoute.snapshot.paramMap.get('id');
+        this.getEmployee();
+      }
+    })
   }
 
   getEmployee(){
@@ -31,5 +43,9 @@ export class EmployeeCardComponent {
         this.employee=employee;
       }
   });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription?.unsubscribe();
   }
 }

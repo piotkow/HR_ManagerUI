7367181e84a3d6b919b5e90363795d3dccaac0c8
Piotr Kowalczyk <piotkow133@student.polsl.pl>
2024-04-07 23:12:48 +0200

    navbar and sidebar sizes, started full employee

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,23 +1,37 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { EmployeeApi, EmployeePositionTeamResponse } from '../../../../libs/api-client';
+import { ImageModule } from 'primeng/image';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ImageModule, TagModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
 
   employeeID: string | null = '';
+  employee?: EmployeePositionTeamResponse;
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private employeeApi: EmployeeApi
   ){}
 
   ngOnInit(){
     this.employeeID = this.activatedRoute.snapshot.paramMap.get('id');
-    console.log("id:",this.employeeID);
+    this.getEmployee();
+  }
+
+  getEmployee(){
+    this.employeeApi.apiEmployeeIdGet({id:Number(this.employeeID)}).subscribe((employee) => {
+      if(employee){
+        this.employee=employee;
+      }
+  });
   }
 }
